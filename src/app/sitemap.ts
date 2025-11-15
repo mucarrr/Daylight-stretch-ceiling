24e0b912fabc22c12b77adbfa0bd21cab79f1bfb
Merve Uçar <mucar2326@gmail.com>
2025-11-15 16:20:11 +0200

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://daylightgergitavan.com'
+  const baseUrl = 'https://www.daylightgergitavan.com'
   
   return [
     {
